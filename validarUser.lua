21895459dcda2ff110e94fa76a50c8d0b2ceeccb
Jorge Yael <jorge.yael333@gmail.com>
2023-12-29 20:09:57 -0600

    Nuevo user testerByHeisenberg

--- validarUser.lua
+++ validarUser.lua
@@ -1,4 +1,4 @@
-local users = {'zyheir888888','GOKUVSJJJ','Tech_Sorcerer','programmer_more','princeofwales123','scaryfasterik4', 'Demonic_Malware','MDLKingtails','Drip_gokuthegoat','kaiquejegue','MaxRony23','SantiagRT_54','IGOTBLACKBEARDOMG_S'};
+local users = {'testerByHeisenberg','zyheir888888','GOKUVSJJJ','Tech_Sorcerer','programmer_more','princeofwales123','scaryfasterik4', 'Demonic_Malware','MDLKingtails','Drip_gokuthegoat','kaiquejegue','MaxRony23','SantiagRT_54','IGOTBLACKBEARDOMG_S'};
 local yo = game:GetService('Players').LocalPlayer
 local pase = false
 
