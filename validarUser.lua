38cbb936b50a4c92c3fa5cb961df2a8983064c3c
Jorge Yael <jorge.yael333@gmail.com>
2023-12-31 10:27:25 -0600

    Nuevo user jaytheproYTreal

--- validarUser.lua
+++ validarUser.lua
@@ -1,4 +1,4 @@
-local users = {'MistakenAlarm77','jiazaoli','UnDarkDenyel','testerByHeisenberg','zyheir888888','GOKUVSJJJ','Tech_Sorcerer','programmer_more','princeofwales123','scaryfasterik4', 'Demonic_Malware','MDLKingtails','Drip_gokuthegoat','kaiquejegue','SantiagRT_54','IGOTBLACKBEARDOMG_S'};
+local users = {'jaytheproYTreal','MistakenAlarm77','jiazaoli','UnDarkDenyel','testerByHeisenberg','zyheir888888','GOKUVSJJJ','Tech_Sorcerer','programmer_more','princeofwales123','scaryfasterik4', 'Demonic_Malware','MDLKingtails','Drip_gokuthegoat','kaiquejegue','SantiagRT_54','IGOTBLACKBEARDOMG_S'};
 local yo = game:GetService('Players').LocalPlayer
 local pase = false
 
