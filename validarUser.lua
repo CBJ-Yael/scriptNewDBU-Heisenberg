8b0cfc317e85c6a3d0d41a67b7c28a383b22bc14
Jorge Yael <jorge.yael333@gmail.com>
2024-01-02 11:17:32 -0600

    Nuevo useR: Imameall

--- validarUser.lua
+++ validarUser.lua
@@ -1,4 +1,4 @@
-local users = {'JD_Game12','puppu520','Dakshsoni890870','meuszo','nitmare999999missed','Reekennedy1057','jaytheproYTreal','MistakenAlarm77','jiazaoli','UnDarkDenyel','testerByHeisenberg','zyheir888888','GOKUVSJJJ','Tech_Sorcerer','programmer_more','princeofwales123','Urmady0u', 'Demonic_Malware','MDLKingtails','Drip_gokuthegoat','kaiquejegue','SantiagRT_54','IGOTBLACKBEARDOMG_S'};
+local users = {'Imameall','JD_Game12','puppu520','Dakshsoni890870','meuszo','nitmare999999missed','Reekennedy1057','jaytheproYTreal','MistakenAlarm77','jiazaoli','UnDarkDenyel','testerByHeisenberg','zyheir888888','GOKUVSJJJ','Tech_Sorcerer','programmer_more','princeofwales123','Urmady0u', 'Demonic_Malware','MDLKingtails','Drip_gokuthegoat','kaiquejegue','SantiagRT_54','IGOTBLACKBEARDOMG_S'};
 local yo = game:GetService('Players').LocalPlayer
 local pase = false
 
