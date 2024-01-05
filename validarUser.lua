53227fb7816fab4a6ea6d7735374f90570968c2c
Jorge Yael <jorge.yael333@gmail.com>
2024-01-05 08:50:12 -0600

    Nuevo user jupiter4513

--- validarUser.lua
+++ validarUser.lua
@@ -1,4 +1,4 @@
-local users = {'lazyhuffboy','Ziyadh_5','GrandMasterMikoPiko','CU19825','nolimtj406','Theyhate_Xavier2','Imameall','JD_Game12','puppu520','Dakshsoni890870','meuszo','nitmare999999missed','Reekennedy1057','jaytheproYTreal','MistakenAlarm77','jiazaoli','UnDarkDenyel','testerByHeisenberg','zyheir888888','GOKUVSJJJ','Tech_Sorcerer','programmer_more','princeofwales123','Urmady0u', 'Demonic_Malware','MDLKingtails','Drip_gokuthegoat','kaiquejegue','SantiagRT_54','IGOTBLACKBEARDOMG_S'};
+local users = {'jupiter4513','lazyhuffboy','Ziyadh_5','GrandMasterMikoPiko','CU19825','nolimtj406','Theyhate_Xavier2','Imameall','JD_Game12','puppu520','Dakshsoni890870','meuszo','nitmare999999missed','Reekennedy1057','jaytheproYTreal','MistakenAlarm77','jiazaoli','UnDarkDenyel','testerByHeisenberg','zyheir888888','GOKUVSJJJ','Tech_Sorcerer','programmer_more','princeofwales123','Urmady0u', 'Demonic_Malware','MDLKingtails','Drip_gokuthegoat','kaiquejegue','SantiagRT_54','IGOTBLACKBEARDOMG_S'};
 local yo = game:GetService('Players').LocalPlayer
 local pase = false
 
