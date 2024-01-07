3f027ca317829192d110f23f1067c042169292b8
Jorge Yael <jorge.yael333@gmail.com>
2024-01-07 16:47:44 -0600

    Farm medio actualizado v2.3.0

--- farmMedio.lua
+++ farmMedio.lua
@@ -1,13 +1,1 @@
-local function validarUrl(enlace)
-    local succes,fallo = pcall(function()
-        loadstring(game:HttpGet(enlace))()
-    end)
-    if fallo then
-        error('fallo el archivo '..fallo)
-    end
-end
-local function validarFarm()
-    return 'https://raw.githubusercontent.com/CBJ-Yael/scriptNewDBU-Heisenberg/main/farm-premium.lua'
-end
-
-validarUrl(validarFarm())
+loadstring(game:HttpGet('https://raw.githubusercontent.com/CBJ-Yael/scriptNewDBU-Heisenberg/main/farm-premium.lua'))()
